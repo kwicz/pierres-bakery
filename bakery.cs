46a10078552ac45b57d78581fd72314724d68733
Katy Solovewicz <katy@solovewicz.com>
2020-02-28 08:39:57 -0800

    add models folder and models namespace

--- bakery.cs
+++ bakery.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Collections.Generic;
+using Bakery.Models;
 
 namespace Bakery
 {
-    public class Store
+  public class Store
+  {
+    public static void Main()
     {
-        
+      Printer.Welcome();
     }
+  }
 }
